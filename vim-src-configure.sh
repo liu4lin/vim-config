eb4d2ddf1d6226431f847d100d2823aeb4490ab9
Shilin Liu <liu4lin@gmail.com>
2021-06-22 16:13:40 +0800

    Create vim-src-configure.sh

---
+++ vim-src-configure.sh
@@ -0,0 +1,1 @@
+./configure --prefix=/data2/liushilin/local/ --enable-python3interp=yes --with-python3-command=/data2/liushilin/local/bin/python3 --enable-pythoninterp=yes --with-python-command=/usr/bin/python2 CPPFLAGS="-I/data2/liushilin/local/include/python3.7m" LDFLAGS="-L/data2/liushilin/local/lib"
