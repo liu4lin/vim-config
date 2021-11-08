7e78132d3bec77954f395b3b6771d077927bc0d0
Shilin Liu <liu4lin@gmail.com>
2021-11-08 17:42:29 +0800

    Update vim-src-configure.sh

--- vim-src-configure.sh
+++ vim-src-configure.sh
@@ -1,3 +1,4 @@
 echo "sudo apt-get install python2.7-dev"
 echo "install python3.7.10"
 ./configure --prefix=/data2/liushilin/local/ --enable-python3interp=yes --with-python3-command=/data2/liushilin/local/bin/python3 --enable-pythoninterp=yes --with-python-command=/usr/bin/python2 CPPFLAGS="-I/data2/liushilin/local/include/python3.7m" LDFLAGS="-L/data2/liushilin/local/lib"
+./configure --prefix=/usr/local/ --enable-python3interp=yes --with-python3-command=/usr/bin/python3 --enable-pythoninterp=yes --with-python-command=/usr/bin/python2 CPPFLAGS="-I/usr/include/python2.7" LDFLAGS="-L/usr/lib/python2.7/config-aarch64-linux-gnu/"
